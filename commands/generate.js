57c55a0131627249963a157dfa10652df72967b4
Chris Parnin <chris.parnin@gmail.com>
2020-06-10 14:01:47 -0400

    wip: generate from index

--- commands/generate.js
+++ commands/generate.js
@@ -1,11 +1,13 @@
-const chalk = require('chalk');
 const fs    = require('fs');
-const os    = require('os');
-
-const child = require('child_process');
 const path  = require('path');
 
+const chalk = require('chalk');
+const yaml = require('js-yaml');
 const cheerio = require('cheerio');
+let marked = require('marked');
+let Parse = require('../lib/markdown');
+let Mustache = require('mustache');
+
 
 exports.command = 'generate <source> [target]';
 exports.desc = 'Create artifact from markdown';
@@ -19,15 +21,22 @@ exports.builder = yargs => {
 exports.handler = async argv => {
     let { source, target } = argv;
 
+    // Setup target defaults
     if( target === undefined )
     {
         target = path.basename(source) + '.html';
     }
+    let target_dir = path.dirname( target );
+    if( fs.existsSync( target ) && fs.lstatSync(target).isDirectory() )
+    {
+        // our target is a directory, not a file.
+        target_dir = target;
+    }
+    console.log(`target dir: ${target_dir}`);
 
     // Prepare css dependencies and copy to target directory
     let github_css = path.join( __dirname, '..', 'node_modules', 'github-markdown-css', 'github-markdown.css');
     let highlight_css = path.join(__dirname, '..', 'node_modules', 'highlight.js', 'styles', 'github.css');
-    let target_dir = path.dirname( target );
     if( !fs.existsSync( target_dir) )
     {
         fs.mkdirSync( target_dir );
@@ -40,18 +49,106 @@ exports.handler = async argv => {
 
     (async () => {
     
-        await generate(source, target, css);
+        // check if source is index
+        if( source.endsWith(".yml") )
+        {
+            await book(source, target_dir, css);
+        }
+        else
+        {
+            await generate(source, target, css);
+        }
 
     })();
 
 };
+
+// Process index file
+async function book(file, target_dir, css)
+{
+    const indexYml = yaml.safeLoad(fs.readFileSync(file), 'utf8');
+
+    await index(file, indexYml, path.join(target_dir, 'index.html'));
+
+    // process chapter content
+    for( var chap of indexYml.chapters )
+    {
+        for( var content of chap.content )
+        {
+            console.log( chap, content );
+            if( typeof(content) == "string" )
+            {
+                let source = path.join( path.dirname(file), content );
+                let target = path.join( target_dir, path.basename(source) + '.html');
+
+                console.log( source, target);
+                await generate(source, target, css )
+            }
+        }
+    }
+
+}
+
+
+async function index(file,indexYml, target)
+{
+    let parser = new Parse();
+
+    var view = {
+        title: indexYml.title,
+        author: indexYml.author,
+        chapters: []
+    };
+
+    var template = 
+    `
+# {{title}}
+By {{author}}
+
+{{#chapters}}
+* **{{name}}**:  _{{about}}_.  
+{{#content}}
+  - {{title}}
+{{/content}}      
+{{/chapters}}    
+`;
+
+    for( var chap of indexYml.chapters  )
+    {
+        let ch = {name: chap.name, about: chap.about, content: []}
+        view.chapters.push( ch );
+
+        for( var content of chap.content )
+        {
+            console.log( chap, content );
+            if( typeof(content) == "string" )
+            {
+                let source = path.join( path.dirname(file), content );
+
+                let html = await parser.parse(source);
+                let $$ = cheerio.load(html);
+                let title = $$('h2')[0].value;
+                ch.content.push( {title: title});
+            }
+        }
+    }
+
+    var output = Mustache.render(template, view);
+    console.log(output);
+    let html = await marked( output );
+
+    console.log(html);
+    let $ = cheerio.load('<!doctype html>' + html);
+
+    fs.writeFileSync(target, $.html());
+
+}
 
 async function generate(source, target, css)
 {
   console.log(chalk.keyword('blue')(`Transforming ${source} => ${target}`));
   // child.execSync(`pandoc --from=markdown_mmd+yaml_metadata_block+smart --standalone --to=html -V css=${css} --output=Shells.html ${source}`)
 
-  let Parse = require('../lib/markdown');
   let parser = new Parse();
 
   console.log(chalk.keyword('green')(`Parsing markdown`));
